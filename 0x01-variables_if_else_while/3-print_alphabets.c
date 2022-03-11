e30416172fdd95dde816cbcf2e0ac00e527a451b
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-11 16:16:42 +0300

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower and uppercase
+ *
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+    char c;
+    for(c = 'a'; c < 'z'; c++) {
+        putchar(c);
+    }
+    for(c = 'A'; c < 'Z'; c++) {
+        putchar(c);
+    }
+    putchar('\n');
+    return (0);
+}
