9ece10684e9215802cdb0a6ee319ee57a8ac152e
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-11 15:58:11 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char a_z;
+
+	for (a_z = 'a'; a_z <= 'z'; a_z++)
+		putchar(a_z);
+		putchar('\n');
+
+return (0);
+}
