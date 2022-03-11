e6e3d1952490fc59af7cebf2fc9ee9cd1403dca6
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-11 17:39:40 +0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * 
+ * Return: 0
+ **/
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
