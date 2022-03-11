a66e2dabfa2ab3314ec2397f61d8423382bf4f76
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-11 17:37:58 +0300

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i % 10 + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
