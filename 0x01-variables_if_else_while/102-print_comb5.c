a154c5d1af2668f3b381d66f7b8c94ac5bdbb165
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-11 21:02:13 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	int i, j;
 
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
