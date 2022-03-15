399d189f1c0b71373a68eaa41d0aa6ec9f73236d
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-15 17:00:35 +0300

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day 00:00 to 23:59
+ * @void - no argument
+ */
+void jack_bauer(void)
+{
+	int i, j;
 
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
