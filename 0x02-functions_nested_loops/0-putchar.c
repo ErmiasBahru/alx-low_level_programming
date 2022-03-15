b20e2752b3941c69aecefeb209963dd266866f83
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-15 16:28:11 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
+#include "main.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+	char alx[] = "Alx";
 
+	int c;
+
+	for (c = 0; c < 9; c++)
+	{
+		_putchar(alx[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
