abdddce9840b196dbe38cdef408316343a2eaf05
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-15 17:02:51 +0300

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,36 @@
+#include "holberton.h"
+/**
+ * times_table - function that prints the 9 times table, starting with 0
+ * @void: no argument
+ */
+void times_table(void)
+{
+	int i, j, k;
 
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			else if (k <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
