0dcb940d1c9bdea0daef1619c884622d8c3fb187
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-15 16:52:00 +0300

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,17 @@
-
+#include "main.h"
+/**
+ * _islower - returns 1 if lowercase alphabet and 0 otherwise
+ *
+ * Return: 0 for uppercase, 1 for lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
