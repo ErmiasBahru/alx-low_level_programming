0d0aa3a74a84608537361da1ab77116a83559c22
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-15 22:41:43 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int counter = 2;
 
+	float a = 1;
+	float b = a + 1;
+	float c = a + b;
+
+	printf("%.0f, ", a);
+	printf("%.0f, ", b);
+	while (counter < 98)
+	{
+		counter++;
+		printf("%.0f", c);
+		a = b;
+		b = c;
+		c = a + b;
+		if (counter < 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
