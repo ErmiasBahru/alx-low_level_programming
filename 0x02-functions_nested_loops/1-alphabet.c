40ba32175f3490466e80da4941bee42206475350
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-15 16:40:51 +0300

    aphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ **/
+void print_alphabet(void)
+{
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
