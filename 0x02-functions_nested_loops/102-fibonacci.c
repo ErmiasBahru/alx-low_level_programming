66244e57b5382828907fec5786e612eb4139a9c4
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-15 22:19:24 +0300

    102 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+/**
+ * main -  prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * @void: no argument
+ * Return: 0
+ */
+int main(void)
+{
+	long pri = 1, sec = 2, sum;
 
+	printf("1, 2");
+	while (sum < 20365011073)
+	{
+		sum = pri + sec;
+		printf(", %lu", sum);
+		pri = sec;
+		sec = sum;
+	}
+	printf("\n");
+	return (0);
+}
