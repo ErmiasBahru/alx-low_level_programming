e0776389835da8292f9990694246af143b8aa51f
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-21 21:04:35 +0300

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "mainn.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: Pointer input
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
