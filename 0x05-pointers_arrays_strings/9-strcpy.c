99b6d15cc1e0c7860a7b98b7c8e045cc17a1d56d
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-21 21:43:14 +0300

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* _strcpy - function that copies the string pointed to by src
+* @dest: input
+* @src: input
+* Return: value the pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int i;
+
+	while (src[l] != '\0')
+	{
+		l++;
+	}
+	for (i = 0; i <= l; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
