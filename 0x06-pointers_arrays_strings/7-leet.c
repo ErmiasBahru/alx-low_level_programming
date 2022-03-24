42892cf5cd0876065d85f6bada7b761c9b41a9bf
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-24 20:55:49 +0300

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337.
+ * @s: input
+ * Return: output
+ */
+
+char *leet(char *s)
+{
+	char alphaArr[] = "a4A4e3E3o0O0t7T7l1L1";
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alphaArr[j] != '\0'; j++)
+		{
+			if (s[i] == alphaArr[j])
+			{
+				s[i] = alphaArr[j + 1];
+				break;
+			}
+		}
+	}
+	return (s);
+}
