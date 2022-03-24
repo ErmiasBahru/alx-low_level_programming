a9c0feed1139d97511fcacb15885aba4406a1884
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-24 20:56:41 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,5 +8,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *rot13(char *);
+void print_number(int n);
 
 #endif
