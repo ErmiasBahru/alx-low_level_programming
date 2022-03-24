16a0c288756ae2d308a4856936f944159dfb65a2
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-24 21:14:28 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,27 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_number - function that prints an integer
- * @n:integer to convert to character
+ * main - main function
  *
+ * Return: Always 0 (Success)
  */
-void print_number(int n)
+int main(void)
 {
-	unsigned int a = n;
+	int n;
+	int a[5];
+	int *p;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		a = -a;
-	}
-	if (a / 10)
-	{
-		print_number(a / 10);
-	}
-	_putchar(a % 10 + '0');
+	a[2] = 1024;
+	p = &n;
+/*
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+	*(p + 5) = 98;
+/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
