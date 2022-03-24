4e32411a1ccfb51fe1f101a612d91befe4f0367a
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-24 21:22:22 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *rot13(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
