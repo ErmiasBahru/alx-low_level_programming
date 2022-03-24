d502e8213d3b104dda8da38bec478653b9369b90
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-24 21:24:53 +0300

    Create 103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * infinite_add - function that adds two numbers
+ *
+ * @r: Buffer where to write the result
+ * @n: Buffer size
+ *
+ * Return: Pointer.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int n)
+{
+	int len1 = 0, len2 = 0;
+	int add = 0;
+	int i = n - 2;
+
+
+	while (n1[len1 + 1] != 0)
+		len1++;
+	while (n2[len2 + 1] != 0)
+		len2++;
+	r[n - 1] = 0;
+
+	while (i >= 0 && (len1 >= 0 || len2 >= 0))
+	{
+		add += (len1 < 0 ? '0' : n1[len1]) + (len2 < 0 ? '0' : n2[len2]);
+		add -= 2 * '0';
+		r[i] = add % 10 + '0';
+		add /= 10;
+		i--;
+		len1--;
+		len2--;
+	}
+
+	if ((i < len1 || i < len2) || (i < 0 && add))
+		return (0);
+
+	add ? r[i] = add + '0' : 1;
+	i += add ? 0 : 1;
+
+	return (r + i);
+}
