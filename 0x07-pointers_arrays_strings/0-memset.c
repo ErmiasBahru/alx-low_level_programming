8f5b6e2b1afb94c44654fa7260727698122d926c
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-28 15:38:09 +0300

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - sets memory to array
+ * @s: array to set
+ * @b: value to set it as
+ * @n: n amount of times
+ * Return: char value of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count < n; count++)
+		s[count] = b;
+	return (s);
+}
