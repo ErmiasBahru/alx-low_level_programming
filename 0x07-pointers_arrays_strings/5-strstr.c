08540f32c04e5aa06a822eb7bdefdfd5c10af0dd
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-28 16:02:03 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,23 +10,21 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 	int j;
-  
-	if (needle[0] == '\0')
-		return (haystack);
+	int k = 0;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[i] == needle[0])
+		j = 0;
+		k = i;
+		while (haystack[k] == needle[j] && needle[j] != '\0')
 		{
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-					break;
-			}
-			if (needle[j] == '\0')
-				return (haystack + i);
+			j++;
+			k++;
 		}
-
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
 	}
-	return (NULL);
+	return (0);
 }
