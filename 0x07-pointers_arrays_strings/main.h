8966bebf1efe0740edea01b4b3c561cda27439fe
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-28 15:55:02 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
