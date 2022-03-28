d880eacd477e97c5f7bcf48547eee862a2ef3d3e
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-28 22:39:28 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
