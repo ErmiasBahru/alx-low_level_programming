b74591895186175647cda8dfe68a146f63239221
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-30 22:47:29 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -37,7 +37,7 @@ int checker(char *s, int x)
  * is_palindrome - function that returns 1 if a string is a palindrome
  * and 0 if not.
  * @s: string
- * Return:
+ * Return: 1 if a string is a palindrome and 0 if not
  */
 int is_palindrome(char *s)
 {
