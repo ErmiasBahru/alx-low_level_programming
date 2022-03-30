b52616928b657a89f10af2280de0bafbfc0dd199
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-03-30 22:32:30 +0300

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * is_prime_num - helper function
+ * @n: num
+ * @x: num
+ * Return: output
+ */
+int is_prime_num(int n, int x)
+{
+	if (n <= 1 || (n != x && n % x == 0))
+	{
+		return (0);
+	}
+	else if (n == x)
+	{
+		return (1);
+	}
+	return (is_prime_num(n, x + 1));
+}
+/**
+ * is_prime_number - function that returns 1 if the input integer is a prime number, otherwise return 0.
+ * @n: num
+ * Return: output
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_number_aux(n, 2));
+}
