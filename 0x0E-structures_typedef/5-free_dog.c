feb929ed608edf97e65e0e3853e270956f0a6a3a
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-04-11 22:15:54 +0300

    Create 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
