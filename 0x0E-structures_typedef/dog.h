3d13e3843750d04e2d4e379ffa7675f8f566216d
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-04-11 22:04:42 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog's basic info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
