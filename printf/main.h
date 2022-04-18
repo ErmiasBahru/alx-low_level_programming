3a838cb7a6887f7570b3d8ea90c91009615b0a05
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-04-18 09:02:28 +0300

    Update main.h

--- printf/main.h
+++ printf/main.h
@@ -1,28 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format, ...);
-int op_c(va_list al);
-int op_s(va_list al);
-int op_d(va_list al);
-int op_u(va_list al);
-int op_o(va_list al);
-int op_x(va_list al);
-int op_X(va_list al);
-int op_p(va_list al);
-int op_r(va_list al);
-int op_percent(va_list al);
-int _putchar(char c);
-
-/**
- * struct op - Struct
- * @op: input
- * @func: function
- */
-typedef struct op
+typedef struct cformat
 {
-	char *op;
-	int (*func)(va_list al);
-} op_t;
+char *print;
+int (*p)();
+} cf_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int pc(va_list arg);
+int ps(va_list arg);
+int pd(va_list arg);
+int pi(va_list arg);
 
 #endif
