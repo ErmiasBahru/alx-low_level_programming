1f24c44ad43a35166f22c7c5692cec59df89cb4e
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-04-18 08:54:55 +0300

    Update main.h

--- printf/main.h
+++ printf/main.h
@@ -4,4 +4,15 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 
+/**
+ * struct op - Struct
+ * @op: input
+ * @func: function
+ */
+typedef struct op
+{
+	char *op;
+	int (*func)(va_list al);
+} op_t;
+
 #endif
