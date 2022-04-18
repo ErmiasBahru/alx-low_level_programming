e272d2d18931f26f86949eb95b73b560cf1ba71b
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-04-18 08:57:21 +0300

    Update main.h

--- printf/main.h
+++ printf/main.h
@@ -2,6 +2,16 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+int op_c(va_list al);
+int op_s(va_list al);
+int op_d(va_list al);
+int op_u(va_list al);
+int op_o(va_list al);
+int op_x(va_list al);
+int op_X(va_list al);
+int op_p(va_list al);
+int op_r(va_list al);
+int op_percent(va_list al);
 int _putchar(char c);
 
 /**
