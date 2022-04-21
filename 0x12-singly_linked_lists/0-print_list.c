36bee2cf0c8f722ac707e01e235a1264563d1a48
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-04-21 13:07:20 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,26 @@
+#include "lists.h"
+/**
+ * print_list -  prints all the elements of a list_t list
+ * @h: element of the structure
+ * Return: number of nodes
+ */
 
+size_t print_list(const list_t *h)
+{
+	size_t con = 0;
+	const list_t *move = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (move != NULL)
+	{
+		if (move->str)
+			printf("[%d] %s\n", move->len, move->str);
+		else
+			printf("[0] (nil)\n");
+		move = move->next;
+		con++;
+	}
+	return (con);
+}
