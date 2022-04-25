b37052d5105e79993bec142cde8e9bc3a918750f
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-04-25 18:09:31 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
