126801e245b4392d67c1430f0b27f513e84d5aa1
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-04-25 18:07:57 +0300

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of listint_t
+ * @h: takes in a const struct type *h
+ * Return: returns the counter
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t c = 0;
+	const listint_t *move = h;
+
+	if (move == NULL)
+		return (0);
+	while (move != NULL)
+	{
+		printf("%d\n", move->n);
+		move = move->next;
+		c++;
+	}
+	return (c);
+}
