51ebe367978eacf570024258fadac1a0df889ae1
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-05-02 21:34:08 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
