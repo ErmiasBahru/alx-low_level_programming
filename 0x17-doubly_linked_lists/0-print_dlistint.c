1d0cfdb56f0bf5ec358b8c2397fb216964b0eee8
ErmiasBahru <ermiasbahru878@gmail.com>
2022-06-10 09:24:36 +0300

    fix format error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,29 +1,21 @@
 #include "lists.h"
+
 /**
- * print_dlistint - prints all the elements of a
- * dlistint_t list
+ * print_dlistint - prints all the elements of a list
  *
- * @h: head of the list
+ * @h: head pointer
+ *
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
+	size_t num_nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		num_nodes++;
 	}
-
-	return (count);
+	return (num_nodes);
 }
