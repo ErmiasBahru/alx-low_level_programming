1c7b7967c2518838e940b302057c9eba67329101
ErmiasBahru <ermiasbahru878@gmail.com>
2022-06-10 09:19:12 +0300

    fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_dlistint - prints all the elements of a
  * dlistint_t list
@@ -10,21 +9,17 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
-
 	count = 0;
-
 	if (h == NULL)
 		return (count);
 
 	while (h->prev != NULL)
 		h = h->prev;
-
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
