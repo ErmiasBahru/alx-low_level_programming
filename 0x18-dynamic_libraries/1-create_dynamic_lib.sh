d1acdb437810e0e81e659f9eb8e6c46227759221
ErmiasBahru <ermiasbahru878@gmail.com>
2022-06-13 21:13:23 +0300

    initial commit

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc *.c -fPIC -shared -o liball.so
+gcc -c -Wall -Werror -fPIC *.c
+gcc -shared -o liball.so *.o
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,5 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
