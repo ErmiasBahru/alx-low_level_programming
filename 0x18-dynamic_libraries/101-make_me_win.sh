71c17ebda73afffb9f7cd64e76750ef073619caf
Ermias Bahru <98729397+ErmiasBahru@users.noreply.github.com>
2022-06-17 09:07:19 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/winwin.so https://github.com/ermiasbahru/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
-export LD_PRELOAD=/tmp/winwin.so
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+sudo LD_PRELOAD=$LD_LIBRARY_PATH ./gm
